f21f168eacadd862852fc1b934dae5e26690c8ea
Andreas Weirauch <Andreas.Weirauch@msg-life.com>
2019-02-22 18:37:14 +0100

    intermediate version

--- src/main/groovy/tv/jirareps/jirareps.groovy
+++ src/main/groovy/tv/jirareps/jirareps.groovy
@@ -1,13 +1,6 @@
 package tv.jirareps
 
-// JIRA filter of Technik Backlog
-String jql='filter = 32081'
-Integer startAt=0
-Integer maxResults=1000
-Object result = JqlRequest.search(jql, startAt, maxResults, Report.fields)
-println "---------------------------------"
+String result = new BacklogReportController().tbReport()
+println result
 
-println "startAt=" + result.startAt + ",  maxResults=" + result.maxResults + ", total=" + result.total
-Report report = Report.build(result)
-println report
 
